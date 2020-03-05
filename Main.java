e7b50a5ca641b8dd7ca5bf8992ce8fa41b63b4db
gueyebabacar <gueybaba@gmail.com>
2020-03-05 16:47:09 +0100

    change wording

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, babacar sidy!");
+        System.out.println("Hello, icicicci huston!");
     }
 }
 
